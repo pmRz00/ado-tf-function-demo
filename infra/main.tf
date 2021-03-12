e21baf09600de23b26aaba7cb4f9377248480b67
pmRz00 <tpstiller@gmx.de>
2021-03-12 16:44:39 +0000

    added "reserved=true"

--- infra/main.tf
+++ infra/main.tf
@@ -79,6 +79,7 @@ resource "azurerm_app_service_plan" "plan" {
   location = "${azurerm_resource_group.rg.location}"
   resource_group_name = "${azurerm_resource_group.rg.name}"
   kind = "Linux"
+  reserved = true
   sku {
     tier = "Dynamic"
     size = "Y1"
